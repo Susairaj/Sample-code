263a9bc84b68b938b5e9244c2e3bed6a312d1837
Susairaj <susaimaths@gmail.com>
2015-11-19 15:36:05 +0530

    .\xml.py

--- Git/xml.py
+++ Git/xml.py
@@ -5,3 +5,19 @@
 <xpath expr="//field[@name='gender']" position="before">
 	<field name="class_id"  domain="[('batch_id','=', batch_id)]" attrs="{'invisible':['|', ('is_type', 'not in', 'student'),('batch_id', '=', False)],'required':[('is_type', 'in', 'student')]}" />
 </xpath>
+Disable create button using xpath::
+<record>
+        ...
+        <field name="arch" type="xml">
+            <xpath expr='//form[@string="Product"]' position='attributes'>
+                <attribute name="create">false</attribute>
+            </xpath>
+        </field> ,
+    </record>
+	
+Filter today,this week, this month::
+<filter string="Today" domain="[('create_date','>=', ((context_today()).strftime('%Y-%m-%d'))), ('create_date','&lt;=', ((context_today()).strftime('%Y-%m-%d')))]"/>
+<filter string="This Week" domain="[('create_date','&gt;=', ((context_today()).strftime('%%Y-%%m-%%d'))),
+ ('create_date','&lt;=', ((context_today()+datetime.timedelta(days=6)).strftime('%%Y-%%m-%%d')))]"/>
+<filter string="This Month" domain="[('create_date','&lt;=',(context_today()+relativedelta(day=31)).strftime('%%Y-%%m-%%d')),
+('create_date','&gt;=',(context_today()-relativedelta(day=1)).strftime('%%Y-%%m-%%d'))]"/>
